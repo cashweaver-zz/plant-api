cc5f50ad558467b392f862eb1d6bef61671d030e
cbweaver <cashbweaver@gmail.com>
2016-08-25 17:24:35 -0700

    Refactor to align with json-api

---
+++ server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+let apiConfig = {
+  prefix: '/api',
+  name: 'Plant API'
+}
+let express = require('express');
+let API = require('json-api');
+let APIError = API.types.Error;
+let mongoose = require('mongoose');
+let models = {
+  Plant: require('./models/plant')
+};
+
+mongoose.connect('mongodb://localhost/plantapi');
+
+// Register models with json-api
+let adapter = new API.dbAdapters.Mongoose(models);
+let registry = new API.ResourceTypeRegistry(
+  { plants: require('./resource-descriptions/plants') },
+  { dbAdapter: adapter }
+);
+
+// Initialize automatic documentation
+let Controller = new API.controllers.API(registry);
+let Docs = new API.controllers.Documentation(registry, {name: apiConfig.name})
+
+// Initialize the express app and front controller
+let app = express();
+let Front = new API.httpStrategies.Express(Controller, Docs);
+let apiReqHandler = Front.apiRequest.bind(Front);
+
+// Routes
+let router = express.Router();
+
+// This is run for every API call. It's included for debugging.
+router.use(function (req, res, next) {
+  console.log('Request received.');
+  next();
+});
+
+router.get('/', Front.docsRequest.bind(Front));
+router.route('/:type(plants)')
+  .get(apiReqHandler);
+
+router.use(function (req, res, next) {
+  Front.sendError(new APIError(404, undefined, 'Not Found'), req, res);
+});
+
+app.use(apiConfig.prefix, router);
+
+// Start the server
+app.listen(3000, function () {
+  let host = server.address().address;
+  console.log('Listening at http://%s:%s', host, port);
+});
