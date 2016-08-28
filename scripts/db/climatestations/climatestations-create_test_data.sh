bbfe4456ead2829aaadc88f988a6cd41daa33695
cbweaver <cashbweaver@gmail.com>
2016-08-27 22:51:17 -0700

    Add test data helper script

---
+++ scripts/db/climatestations/climatestations-create_test_data.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Download datasets from the NCDC
+
+DATA_PATH="/home/vagrant/plant-api/data/climatestations/raw"
+cd "$DATA_PATH"
+if [[ "$(pwd)" != "$DATA_PATH" ]]
+then
+  echo "Problem changing directory to $DATA_PATH"
+  exit 1;
+fi
+
+# Average
+head -n12 dly-tavg-normal.txt > dly-tavg-normal-test.txt
+head -n12 dly-tavg-stddev.txt > dly-tavg-stddev-test.txt
+# Minimum
+head -n12 dly-tmax-normal.txt > dly-tmax-normal-test.txt
+head -n12 dly-tmax-stddev.txt > dly-tmax-stddev-test.txt
+# Maximum
+head -n12 dly-tmin-normal.txt > dly-tmin-normal-test.txt
+head -n12 dly-tmin-stddev.txt > dly-tmin-stddev-test.txt
