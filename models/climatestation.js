f45cb04c4631426544ae1cf6033e631e9641879d
cbweaver <cashbweaver@gmail.com>
2016-09-13 11:55:47 -0700

    Add 2dsphere index to climatestations

--- models/climatestation.js
+++ models/climatestation.js
@@ -2,7 +2,49 @@
 let mongoose = require('mongoose');
 
 let schema = new mongoose.Schema({
-  name: String
+  dlyTMaxNormal: {
+    data: [Number],
+    completeness: String,
+  },
+  dlyTMaxStddev: {
+    data: [Number],
+    completeness: String,
+  },
+  dlyTMinNormal: {
+    data: [Number],
+    completeness: String,
+  },
+  dlyTMinStddev: {
+    data: [Number],
+    completeness: String,
+  },
+  location: {
+    ncdc_meta: {
+      gsn_flag: String,
+      hcn_flag: String,
+      wmoid: String,
+      method: String
+    },
+    //address : {
+      //formatted: String
+    //},
+    lnglat: {
+      type: {type: String},
+      coordinates: {type: [Number], index: '2dsphere'}
+    }
+  },
+  stationId: String,
+  frostDates: {
+    90: [Number],
+    95: [Number],
+    99: [Number]
+  },
+  dlySoilTAvg: {
+    data: [Number],
+    completeness: String,
+  }
 });
 
-module.exports = mongoose.model('Station', schema);
+schema.index({'location.lnglat': '2dsphere'});
+
+module.exports = mongoose.model('Climatestation', schema);
--- scripts/db/db-import.sh
+++ scripts/db/db-import.sh
@@ -8,3 +8,5 @@ echo "  plants"
 mongoimport --drop --db plantapi --collection plants $DATA_PATH/plants/processed/plants.json
 echo "  climatestations"
 mongoimport --drop --db plantapi --collection climatestations $DATA_PATH/climatestations/processed/climatestations.json
+echo "    setting 2dsphere index for station geolocation"
+mongo plantapi --eval "db.climatestations.createIndex({'location.lnglat': '2dsphere'})"
